326e07c8c4393b6b4471da335604e551486ade6f
bobi1234 <jxjy.ing@foxmail.com>
2018-07-30 20:25:40 +0800

    添加服务配置客户端

---
+++ cms-order/src/main/java/cn/xlbweb/cms/controller/YmlController.java
@@ -0,0 +1,24 @@
+package cn.xlbweb.cms.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: bobi
+ * @date: 2018/7/30 15:05
+ * @description:
+ */
+@RestController
+@RequestMapping("/yml")
+public class YmlController {
+
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/env")
+    public String getEnv() {
+        return env;
+    }
+}
