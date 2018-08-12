ed670a4a076e75ec3a229d0852100d379bff5e87
bobi <jxjy.ing@foxmail.com>
2018-08-12 19:50:24 +0800

    配置中心客户端从Gitee上拉取配置文件成功

--- cms-order/src/main/java/cn/xlbweb/cms/controller/YmlController.java
+++ cms-order/src/main/java/cn/xlbweb/cms/controller/YmlController.java
@@ -8,17 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author: bobi
  * @date: 2018/7/30 15:05
- * @description:
+ * @description: 测试从Gitee上拉取下来的配置文件是否成功
  */
 @RestController
 @RequestMapping("/yml")
 public class YmlController {
 
-//    @Value("${env}")
-//    private String env;
-//
-//    @GetMapping("/env")
-//    public String getEnv() {
-//        return env;
-//    }
+    @Value("${env}")
+    private String env;
+
+    @GetMapping("/env")
+    public String getEnv() {
+        return env;
+    }
 }
